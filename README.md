d50f28ad91ea7696b976520ba3fa9b101b57c73a
Aidan Feldman <aidan.feldman@gmail.com>
2017-09-18 23:01:53 -0400

    add instructions about how to start the server

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ website
 =======
 
 website for Empire Conferences 2017
+
+## Running locally
+
+1. Install [Yarn](https://yarnpkg.com/).
+1. Install dependencies.
+
+    ```sh
+    yarn
+    ```
+
+1. Start the server.
+
+    ```sh
+    node server.js
+    ```
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var st = require('st');
 var http = require('http');
 
+var PORT = process.env.PORT || 6090;
+
 http.createServer(
   st({
     path: process.cwd(),
     url: '/',
     index: 'index.html'
   })
-).listen(process.env.PORT || 6090)
+).listen(PORT, function() {
+  console.log("Server running at http://localhost:" + PORT);
+});
